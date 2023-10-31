ed3397889fbf6c0a32a47a66678266bbdac7dc4a
Amanpreet Singh Saimbhi <aman.saimbhi@gmail.com>
2023-10-31 21:11:02 +0000

    sort boxes with start time

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 import json
+from transform import *
 
 app = Flask(__name__)
 
@@ -12,6 +13,9 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     data = json.loads(request.data, strict=False)
+    # Sort the objects according to their start time.
+    sorted_boxes = sorted(data['box_objects'], key=lambda x: x['start_time'])
+    create_sdl_from_ui_inputs(sorted_boxes)
     return "Success"
 
 # run the application
