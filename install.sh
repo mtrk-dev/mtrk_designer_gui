63343ebb474b5a60eb79c7da9db3402287c3b1b5
Amanpreet Singh Saimbhi <aman.saimbhi@gmail.com>
2024-04-26 20:18:05 +0000

    add viewer setup to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 echo "------------ Running the install script ---------------"
+
+# setting up nginx and hosting static files for viewer
+sudo apt update
+sudo apt install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo ufw allow 'Nginx Full'
+sudo cp -r /opt/mtrk_viewer /var/www/mtrk_viewer
+sudo cp /opt/mtrk_viewer/mtrk_viewer_nginx.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/mtrk_viewer_nginx.conf /etc/nginx/sites-enabled
+sudo systemctl reload nginx
+
 cd /opt/mtrk_designer_gui
 
 # Add local folder to PATH
@@ -10,7 +22,7 @@ pip install virtualenv
 virtualenv ./venv
 source ./venv/bin/activate
 
-pip install -r requirements.txt
+pip install --isolated -r requirements.txt
 
 echo "----------------- Installed requiremenents --------------------------"
 echo "--------------- Starting the MTRK web app service -------------------"
